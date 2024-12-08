06c01a53245d357421a65eeac07e76763f4b068a
Jayreddin <jayreddin@hotmail.com>
2024-12-08 17:54:30 +0000

    r the commit message for your changes. Lines starting

--- server/routes/search.js
+++
@@ -1,7 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const searchController = require('../controllers/searchController');
-
-router.post('/getSocialMediaAccounts', searchController.getSocialMediaAccounts);
-
-module.exports = router;
